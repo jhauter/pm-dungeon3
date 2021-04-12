d24a6037791c2ae01ea34c39706f31d6a94173a4
Andreas <andreaswegner@posteo.de>
2021-04-12 23:10:53 +0200

    Set up project files
    Added src, lib including pmdungeon.jar, assets and additional textures

---
+++ src/TestMain.java
@@ -0,0 +1,9 @@
+import de.fhbielefeld.pmdungeon.vorgaben.game.Controller.MainController;
+
+public class TestMain extends MainController
+{
+	public static void main(String[] args)
+	{
+		de.fhbielefeld.pmdungeon.desktop.DesktopLauncher.run(new TestMain());
+	}
+}
