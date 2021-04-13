2e71bff33e9c017b78bd12dde1dc4caddb76e130
awegner4 <andreas.wegner@fh-bielefeld.de>
2021-04-13 15:45:12 +0200

    Update TestMain.java

--- src/TestMain.java
+++ src/TestMain.java
@@ -4,6 +4,7 @@ public class TestMain extends MainController
 {
 	public static void main(String[] args)
 	{
+		//Helloooooooo
 		de.fhbielefeld.pmdungeon.desktop.DesktopLauncher.run(new TestMain());
 	}
 }
