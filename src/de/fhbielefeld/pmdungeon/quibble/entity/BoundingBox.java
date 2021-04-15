17240a9e54d7180bf671e9d6223f39c7ea2d3080
Andreas <andreaswegner@posteo.de>
2021-04-15 17:16:38 +0200

    Added BoundingBox class to help with complex collision somewhen

---
+++ src/de/fhbielefeld/pmdungeon/quibble/entity/BoundingBox.java
@@ -0,0 +1,17 @@
+package de.fhbielefeld.pmdungeon.quibble.entity;
+
+public class BoundingBox
+{
+	public final float x;
+	public final float y;
+	public final float width;
+	public final float height;
+	
+	public BoundingBox(float x, float y, float width, float height)
+	{
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+}
