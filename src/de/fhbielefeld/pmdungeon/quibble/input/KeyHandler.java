4699150aaaaec0c4702bd2d7fbf3202111485b57
mkanders <malte_theodor.kanders@fh-bielefeld.de>
2021-04-15 12:43:18 +0200

    fix: Handler is initialized only once.

--- src/de/fhbielefeld/pmdungeon/quibble/input/KeyHandler.java
+++ src/de/fhbielefeld/pmdungeon/quibble/input/KeyHandler.java
@@ -2,6 +2,7 @@ package de.fhbielefeld.pmdungeon.quibble.input;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.function.Supplier;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -10,20 +11,20 @@ public class KeyHandler implements InputHandler {
 
 	private ArrayList<InputListener> listener = new ArrayList<>();
 	
-	private final boolean UP = (Gdx.input.isKeyPressed(Input.Keys.W));
-	private final boolean RIGHT = (Gdx.input.isKeyPressed(Input.Keys.D));
-	private final boolean DOWN = (Gdx.input.isKeyPressed(Input.Keys.S));
-	private final boolean LEFT = (Gdx.input.isKeyPressed(Input.Keys.A));
+	private final Supplier<Boolean> UP = () -> Gdx.input.isKeyPressed(Input.Keys.W);
+	private final Supplier<Boolean> DOWN = () -> Gdx.input.isKeyPressed(Input.Keys.S);
+	private final Supplier<Boolean> RIGHT = () -> Gdx.input.isKeyPressed(Input.Keys.D);
+	private final Supplier<Boolean> LEFT = () -> Gdx.input.isKeyPressed(Input.Keys.A);
 	
-	private final ArrayList<Boolean> straightDirectionIsPressed = new ArrayList<>(Arrays.asList(UP, RIGHT, DOWN, LEFT));
+	private final ArrayList<Supplier<Boolean>> straightDirectionIsPressed = new ArrayList<>(Arrays.asList(UP, RIGHT, DOWN, LEFT));
 	private final ArrayList<KEY> straightDirectionKEY = new ArrayList<>(Arrays.asList(KEY.UP, KEY.RIGHT, KEY.DOWN, KEY.LEFT));
 	
-	private final boolean UP_RIGHT = (Gdx.input.isKeyPressed(Input.Keys.W) && (Gdx.input.isKeyPressed(Input.Keys.D)));
-	private final boolean DOWN_RIGHT = (Gdx.input.isKeyPressed(Input.Keys.S) && (Gdx.input.isKeyPressed(Input.Keys.D)));
-	private final boolean DOWN_LEFT = (Gdx.input.isKeyPressed(Input.Keys.S) && (Gdx.input.isKeyPressed(Input.Keys.A)));
-	private final boolean UP_LEFT = (Gdx.input.isKeyPressed(Input.Keys.W) && (Gdx.input.isKeyPressed(Input.Keys.A)));
+	private final Supplier<Boolean> UP_RIGHT = () -> Gdx.input.isKeyPressed(Input.Keys.W) && Gdx.input.isKeyPressed(Input.Keys.D);
+	private final Supplier<Boolean> DOWN_RIGHT = () -> Gdx.input.isKeyPressed(Input.Keys.S) && (Gdx.input.isKeyPressed(Input.Keys.D));
+	private final Supplier<Boolean> DOWN_LEFT = () -> Gdx.input.isKeyPressed(Input.Keys.S) && (Gdx.input.isKeyPressed(Input.Keys.A));
+	private final Supplier<Boolean> UP_LEFT = () -> Gdx.input.isKeyPressed(Input.Keys.W) && (Gdx.input.isKeyPressed(Input.Keys.A));
 
-	private final ArrayList<Boolean> diagonalDirectionIsPressed = new ArrayList<>(Arrays.asList(UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT));
+	private final ArrayList<Supplier<Boolean>> diagonalDirectionIsPressed = new ArrayList<>(Arrays.asList(UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT));
 	private final ArrayList<KEY> diagonalDirectionKEY = new ArrayList<>(Arrays.asList(KEY.UP_RIGHT, KEY.DOWN_RIGHT, KEY.DOWN_LEFT, KEY.UP_LEFT));
 
 
@@ -47,12 +48,13 @@ public class KeyHandler implements InputHandler {
 
 	@Override
 	public KEY updateHandler() {
-		for (int i = 0; i < straightDirectionKEY.size(); i++) {
+		
+		for (int i = 0; i < straightDirectionIsPressed.size(); i++) {
 			for (int j = 0; j < diagonalDirectionKEY.size(); j++) {
-				if(diagonalDirectionIsPressed.get(j))
+				if(diagonalDirectionIsPressed.get(j).get())
 					return diagonalDirectionKEY.get(j);
 			}
-			if(straightDirectionIsPressed.get(i))
+			if((boolean) straightDirectionIsPressed.get(i).get())
 				return straightDirectionKEY.get(i);
 		}
 		return KEY.NO_KEY;
