62504056bf72e950aba56937e022dc75032b9158
Mathis Grabert <lamanzana117@googlemail.com>
2021-04-23 11:26:25 +0200

    Reworked input handler and added hit input

--- src/de/fhbielefeld/pmdungeon/quibble/input/DungeonInput.java
+++ src/de/fhbielefeld/pmdungeon/quibble/input/DungeonInput.java
@@ -1,44 +1,28 @@
 package de.fhbielefeld.pmdungeon.quibble.input;
 
+import java.util.function.Supplier;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
 public enum DungeonInput {
 
-	UP(2, false, 90.0F), UP_RIGHT(2, false, 45.0F), RIGHT(2, false, 0.0F), DOWN_RIGHT(2, false, 315.0F),
-	DOWN(2, false, 270.0F), DOWN_LEFT(2, false, 225.0F), LEFT(2, false, 180.0F), UP_LEFT(2, false, 135.0F),
-	NO_KEY(3, false, 0.0F);
-
-	int priority;
-	boolean justPressed;
-	float angle;
-
-	/**
-	 * Specific buttons constant. Must be assigned to an event and then serves as
-	 * identification.
-	 * 
-	 * @param priority    A value to determine which key is treated with priority.
-	 * @param justPressed Returns a true if the button was not pressed for a long
-	 *                    time but only activated for a short time.
-	 * @param angleThe    angle at which the player is standing to the button.
-	 *                    Starts with the right direction button at 0 and runs
-	 *                    counter-clockwise. Values from 0 to 360
-	 */
-	DungeonInput(int priority, boolean justPressed, float angle) {
-		this.priority = priority;
-		this.justPressed = justPressed;
-		this.angle = angle;
+	UP(() -> Gdx.input.isKeyPressed(Input.Keys.W)),
+	RIGHT(() -> Gdx.input.isKeyPressed(Input.Keys.D)),
+	LEFT(() -> Gdx.input.isKeyPressed(Input.Keys.A)),
+	DOWN(() -> Gdx.input.isKeyPressed(Input.Keys.S)),
+	UP_RIGHT(() -> Gdx.input.isKeyPressed(Input.Keys.W) && Gdx.input.isKeyPressed(Input.Keys.D)),
+	UP_LEFT(() -> Gdx.input.isKeyPressed(Input.Keys.W) && Gdx.input.isKeyPressed(Input.Keys.A)),
+	DOWN_LEFT(() -> Gdx.input.isKeyPressed(Input.Keys.S) && Gdx.input.isKeyPressed(Input.Keys.A)),
+	DOWN_RIGHT(() -> Gdx.input.isKeyPressed(Input.Keys.S) && Gdx.input.isKeyPressed(Input.Keys.D)),
+	HIT(() -> Gdx.input.isButtonJustPressed(Input.Buttons.LEFT));
+	private final Supplier<Boolean> inputCheck;
+	
+	private DungeonInput(Supplier<Boolean> inputCheck) {
+		this.inputCheck = inputCheck;
 	}
-
-	/**
-	 * angle at which the player is standing to the button. Starts with the right
-	 * direction button at 0 and runs counter-clockwise. Values from 0 to 360
-	 * 
-	 * @return Returns the angle represented by the specified button. <br>
-	 *         </br>
-	 *         For Example: <br>
-	 *         KEY.Right.getAngle() returns float value : 0 </br>
-	 *         
-	 *         KEY.Left.getAngle() returns float value: 180 </br>
-	 */
-	public float getAngle() {
-		return this.angle;
+	
+	public boolean isPressed() {
+		return inputCheck.get();
 	}
 }
--- src/de/fhbielefeld/pmdungeon/quibble/input/DungeonInputHandler.java
+++ src/de/fhbielefeld/pmdungeon/quibble/input/DungeonInputHandler.java
@@ -1,38 +1,14 @@
 package de.fhbielefeld.pmdungeon.quibble.input;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Supplier;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 
 public class DungeonInputHandler implements InputHandler {
 
 	private ArrayList<InputListener> listener = new ArrayList<>();
-	
-	private final Supplier<Boolean> UP = () -> Gdx.input.isKeyPressed(Input.Keys.W);
-	private final Supplier<Boolean> DOWN = () -> Gdx.input.isKeyPressed(Input.Keys.S);
-	private final Supplier<Boolean> RIGHT = () -> Gdx.input.isKeyPressed(Input.Keys.D);
-	private final Supplier<Boolean> LEFT = () -> Gdx.input.isKeyPressed(Input.Keys.A);
-	
-	private final List<Supplier<Boolean>> straightDirectionIsPressed = Arrays.asList(UP, RIGHT, DOWN, LEFT);
-	private final List<DungeonInput> straightDirectionKEY = Arrays.asList(DungeonInput.UP, DungeonInput.RIGHT, DungeonInput.DOWN, DungeonInput.LEFT);
-	
-	private final Supplier<Boolean> UP_RIGHT = () -> Gdx.input.isKeyPressed(Input.Keys.W) && Gdx.input.isKeyPressed(Input.Keys.D);
-	private final Supplier<Boolean> DOWN_RIGHT = () -> Gdx.input.isKeyPressed(Input.Keys.S) && (Gdx.input.isKeyPressed(Input.Keys.D));
-	private final Supplier<Boolean> DOWN_LEFT = () -> Gdx.input.isKeyPressed(Input.Keys.S) && (Gdx.input.isKeyPressed(Input.Keys.A));
-	private final Supplier<Boolean> UP_LEFT = () -> Gdx.input.isKeyPressed(Input.Keys.W) && (Gdx.input.isKeyPressed(Input.Keys.A));
-
-	private final List<Supplier<Boolean>> diagonalDirectionIsPressed = Arrays.asList(UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT);
-	private final List<DungeonInput> diagonalDirectionKEY = Arrays.asList(DungeonInput.UP_RIGHT, DungeonInput.DOWN_RIGHT, DungeonInput.DOWN_LEFT, DungeonInput.UP_LEFT);
-
 
 	@Override
 	public void addInputListener(InputListener listener) {
-		if(this.listener.contains(listener))
-		{
+		if (this.listener.contains(listener)) {
 			throw new IllegalArgumentException("this listener was already added");
 		}
 		this.listener.add(listener);
@@ -50,18 +26,12 @@ public class DungeonInputHandler implements InputHandler {
 
 	@Override
 	public void updateHandler() {
-		
-		for (int i = 0; i < straightDirectionIsPressed.size(); i++) {
-			for (int j = 0; j < diagonalDirectionKEY.size(); j++) {
-				if(diagonalDirectionIsPressed.get(j).get()) {
-					this.notifyListeners(diagonalDirectionKEY.get(j));
-					return;
-				}
-			}
-			if(straightDirectionIsPressed.get(i).get()) {
-				this.notifyListeners(straightDirectionKEY.get(i));
-				return;
+		DungeonInput[] inputs = DungeonInput.values();
+		for (DungeonInput i : inputs) {
+			if (i.isPressed()) {
+				notifyListeners(i);
 			}
 		}
+
 	}
 }
