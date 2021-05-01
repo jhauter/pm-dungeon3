ddb76ccc46f3bf3686256dd59090b2d96f2dfee4
Andreas <andreaswegner@posteo.de>
2021-05-01 17:59:07 +0200

    Reverted stats demo in healing potion

--- src/de/fhbielefeld/pmdungeon/quibble/item/ItemHealingPotion.java
+++ src/de/fhbielefeld/pmdungeon/quibble/item/ItemHealingPotion.java
@@ -1,8 +1,6 @@
 package de.fhbielefeld.pmdungeon.quibble.item;
 
 import de.fhbielefeld.pmdungeon.quibble.entity.Creature;
-import de.fhbielefeld.pmdungeon.quibble.entity.battle.CreatureStats;
-import de.fhbielefeld.pmdungeon.quibble.entity.battle.CreatureStatsAttribs;
 
 public class ItemHealingPotion extends Item
 {
@@ -33,14 +31,5 @@ public class ItemHealingPotion extends Item
 	public String getTexture()
 	{
 		return this.texture;
-	}
-	
-	@Override
-	public CreatureStats getItemStats()
-	{
-		CreatureStats s = new CreatureStats();
-		s.setStat(CreatureStatsAttribs.DAMAGE_PHYS, 20.0D);
-		s.setStat(CreatureStatsAttribs.HEALTH, 20.0D);
-		return s;
 	}
 }
