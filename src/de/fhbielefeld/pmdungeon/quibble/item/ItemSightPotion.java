bf75bb84a3328dfca2638ba9c1b71baa8c082fd7
mkanders <malte_theodor.kanders@fh-bielefeld.de>
2021-05-06 21:41:19 +0200

    Added Texture and new Itemclass (Potion of Sight)

---
+++ src/de/fhbielefeld/pmdungeon/quibble/item/ItemSightPotion.java
@@ -0,0 +1,37 @@
+package de.fhbielefeld.pmdungeon.quibble.item;
+
+import de.fhbielefeld.pmdungeon.quibble.entity.Creature;
+import de.fhbielefeld.pmdungeon.quibble.item.visitor.ItemVisitor;
+import de.fhbielefeld.pmdungeon.quibble.trap.Trap;
+
+public class ItemSightPotion extends ItemPotion
+{
+	private final double timeOfSight;
+	
+	/**
+	 * Creates a healing potion item.
+	 * @param displayName user friendly display name
+	 * @param healAmount the amount this potion should heal
+	 * @param texture texture that is used to render the item
+	 */
+	public ItemSightPotion(String displayName, double timeOfSight, String texture)
+	{
+		super(displayName, texture);
+		this.timeOfSight = timeOfSight;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onUse(Creature user)
+	{
+		Trap.PlayerEffect = true;
+	}
+
+	@Override
+	public void accept(ItemVisitor visitor)
+	{
+		visitor.visit(this);
+	}
+}
