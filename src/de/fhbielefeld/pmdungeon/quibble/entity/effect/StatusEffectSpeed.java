5bea838e846a2def57928cd5420bd8d813d937a4
mkanders <malte_theodor.kanders@fh-bielefeld.de>
2021-05-07 11:06:21 +0200

    Implementation of Statuseffect Speed

---
+++ src/de/fhbielefeld/pmdungeon/quibble/entity/effect/StatusEffectSpeed.java
@@ -0,0 +1,27 @@
+package de.fhbielefeld.pmdungeon.quibble.entity.effect;
+
+import de.fhbielefeld.pmdungeon.quibble.entity.Creature;
+import de.fhbielefeld.pmdungeon.quibble.entity.battle.CreatureStats;
+import de.fhbielefeld.pmdungeon.quibble.entity.battle.CreatureStatsAttribs;
+
+public class StatusEffectSpeed extends StatusEffect {
+
+	private double currentNewSpeed;
+
+	/**
+	 * 
+	 * @param creature        the Creature which will be effected
+	 * @param currentNewSpeed the new WalkingSpeed
+	 */
+	public StatusEffectSpeed(Creature creature, double currentNewSpeed) {
+		super(creature);
+		this.currentNewSpeed = currentNewSpeed;
+	}
+
+	@Override
+	public CreatureStats getStatsModification(CreatureStats currentCreatureStats) {
+		currentCreatureStats.setStat(CreatureStatsAttribs.WALKING_SPEED, currentNewSpeed);
+		return super.getStatsModification(currentCreatureStats);
+
+	}
+}
