b86c2463475002187aa85a95d6a0a83dd75d1ac2
mkanders <malte_theodor.kanders@fh-bielefeld.de>
2021-05-12 16:50:06 +0200

    Added javadocs

--- src/de/fhbielefeld/pmdungeon/quibble/quest/Quest.java
+++ src/de/fhbielefeld/pmdungeon/quibble/quest/Quest.java
@@ -5,48 +5,78 @@ import de.fhbielefeld.pmdungeon.quibble.entity.Creature;
 public abstract class Quest {
 
 	public final static String QUEST_TEXTURE_PATH = "assets/textures/quest/";
+	public final static String ACCEPT_DECLINE = "To Accept press J to Decline press N";
 
 	public static final QuestYellowFlag QUEST_YELLOW_FLAG = new QuestYellowFlag("Level Quest", "yellow_flag");
-	
-	
-	
+
 	protected boolean isAccept;
 	protected boolean isActive;
 
 	private final String texture;
 	private final String questName;
-	
+
+	/**
+	 * Abstract real logic Quest
+	 * 
+	 * @param questName name of a certain Quest
+	 * @param texture   the texture which should be used
+	 */
 	public Quest(String questName, String texture) {
 		this.texture = texture;
 		this.questName = questName;
 	}
 
-	public void setAccept(boolean isAccept){
-		this.isAccept = isAccept;
+	/**
+	 * whether a quest has been accepted and has now become active
+	 */
+	protected void setActive() {
+		this.isActive = true;
 	}
-	
-	public boolean isAccept() {
-		return isAccept;
-	}
-	
-	public void setActive(boolean isActiv) {
-		this.isActive = isActiv;
-	}
-	
+
+	/**
+	 * 
+	 * @return whether a quest is active or not
+	 */
 	public boolean isActive() {
 		return isActive;
 	}
 
+	/**
+	 * 
+	 * @return The specific task of the quest as a string
+	 */
 	public abstract String getTask();
 
-	public abstract void onComplete(Creature c);
+	/**
+	 * 
+	 * @return A string that represents how far a quest has already been completed
+	 */
+	public abstract String onWork();
 
-	public abstract void onWork(Creature c);
-	
+	/**
+	 * 
+	 * @return A string that represents what reward the quest will give
+	 */
+	public abstract String onComplete();
+
+	/**
+	 * 
+	 * @param c Creature to receive the specific reward
+	 */
+	public abstract void onReward(Creature c);
+
+	/**
+	 * 
+	 * @return the given Texture
+	 */
 	public final String getTexture() {
 		return this.texture;
 	}
-	
+
+	/**
+	 * 
+	 * @return name of a Quest 
+	 */
 	public final String getQuestName() {
 		return this.questName;
 	}
--- src/de/fhbielefeld/pmdungeon/quibble/quest/QuestMannequin.java
+++ src/de/fhbielefeld/pmdungeon/quibble/quest/QuestMannequin.java
@@ -1,30 +1,70 @@
 package de.fhbielefeld.pmdungeon.quibble.quest;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
 import de.fhbielefeld.pmdungeon.quibble.entity.Entity;
 
 public class QuestMannequin extends Entity {
-	
+
 	private boolean isAccept;
-	
+	private boolean isActive;
+
 	private final Quest quest;
-	
+
+	/**
+	 * Will create a visible Quest Entity
+	 * 
+	 * @param quest the quest which should be visible
+	 * @param x     x float of the Position
+	 * @param y     y float of the Position
+	 */
 	public QuestMannequin(Quest quest, float x, float y) {
 		super(x, y);
 		this.quest = quest;
-		this.animationHandler.addAsDefaultAnimation("default", 1, 1, Quest.QUEST_TEXTURE_PATH + quest.getTexture() + ".png", -1);
+		this.animationHandler.addAsDefaultAnimation("default", 1, 1,
+				Quest.QUEST_TEXTURE_PATH + quest.getTexture() + ".png", -1);
 	}
-	
+
 	@Override
 	public boolean deleteable() {
 		return isAccept;
 	}
-	
+
+	@Override
+	protected void updateLogic() {
+		super.updateLogic();
+		if (isActive) {
+			if (Gdx.input.isKeyJustPressed(Input.Keys.J)) {
+				this.setAccept();
+			}
+			if (Gdx.input.isKeyJustPressed(Input.Keys.N)) {
+				setActive(false);
+			}
+		}
+	}
+
+	/**
+	 * Sets whether the quest can be accepted or rejected
+	 * @param isActive if true the quest can be decline or accept
+	 */
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	/**
+	 * 
+	 * @return the actual real quest of the doll
+	 */
 	public Quest getQuest() {
 		return this.quest;
 	}
-	
+
+	/**
+	 * if set the quest becomes active and the doll disappears
+	 */
 	public void setAccept() {
+		this.quest.setActive();
 		this.isAccept = true;
 	}
-	
 }
