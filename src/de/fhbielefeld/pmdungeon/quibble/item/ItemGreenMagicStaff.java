f5784849d4545b557d06962a228443183afe5f61
mkanders <malte_theodor.kanders@fh-bielefeld.de>
2021-05-17 16:33:08 +0200

    Implementation of two staff's

---
+++ src/de/fhbielefeld/pmdungeon/quibble/item/ItemGreenMagicStaff.java
@@ -0,0 +1,23 @@
+package de.fhbielefeld.pmdungeon.quibble.item;
+
+import de.fhbielefeld.pmdungeon.quibble.entity.battle.CreatureStats;
+import de.fhbielefeld.pmdungeon.quibble.entity.battle.CreatureStatsAttribs;
+
+public class ItemGreenMagicStaff extends ItemWeaponMelee
+{
+	protected ItemGreenMagicStaff(String name, float itemWidth, float itemHeight, float swingSpeed, float visibleTime, String texture)
+	{
+		super(name, itemWidth, itemHeight, swingSpeed, visibleTime, texture);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public CreatureStats getItemStats()
+	{
+		CreatureStats stats = new CreatureStats();
+		stats.setStat(CreatureStatsAttribs.DAMAGE_PHYS, 7.0D);
+		return stats;
+	}
+}
