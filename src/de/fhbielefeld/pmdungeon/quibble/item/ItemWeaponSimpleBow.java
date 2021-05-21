ce2e17f8333c249027a8ec0a51eb6198d764b9b8
mkanders <malte_theodor.kanders@fh-bielefeld.de>
2021-05-21 10:47:09 +0200

    Finished Bow and Arrow

--- src/de/fhbielefeld/pmdungeon/quibble/item/ItemWeaponSimpleBow.java
+++ src/de/fhbielefeld/pmdungeon/quibble/item/ItemWeaponSimpleBow.java
@@ -4,15 +4,26 @@ import de.fhbielefeld.pmdungeon.quibble.entity.Creature;
 import de.fhbielefeld.pmdungeon.quibble.entity.battle.CreatureStats;
 import de.fhbielefeld.pmdungeon.quibble.entity.battle.CreatureStatsAttribs;
 import de.fhbielefeld.pmdungeon.quibble.entity.range_combat.Projectile;
+import de.fhbielefeld.pmdungeon.quibble.entity.range_combat.ShootArrow;
+import de.fhbielefeld.pmdungeon.quibble.entity.range_combat.SpellIceBlast;
 import de.fhbielefeld.pmdungeon.quibble.item.visitor.ItemVisitor;
 
 public class ItemWeaponSimpleBow extends ItemWeaponRange{
 
+	private Projectile arrow;
+	
 	protected ItemWeaponSimpleBow(String name, float itemWidth, float itemHeight, float visibleTime, String texture) {
 		super(name, itemWidth, itemHeight, visibleTime, texture);
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public void onUse(Creature user) {
+		super.onUse(user);
+		setUser(user);
+		user.getLevel().spawnEntity(spawnProjectile(user));
+		user.getCurrentStats().setStat(CreatureStatsAttribs.HIT_COOLDOWN, 15);
+	}
 
 
 	@Override
@@ -25,13 +36,14 @@ public class ItemWeaponSimpleBow extends ItemWeaponRange{
 	public CreatureStats getItemStats() {
 		CreatureStats stats = new CreatureStats();
 		stats.setStat(CreatureStatsAttribs.DAMAGE_PHYS, 2);
-		return super.getItemStats();
+		return stats;
 	}
 
 	@Override
 	public Projectile spawnProjectile(Creature user) {
-		// TODO Auto-generated method stub
-		return null;
+		arrow = new ShootArrow(this.setProjectileStartPoint(0.5f), user);
+		arrow.setVelocityX(setVelocity(1));
+		return arrow;
 	}
 
 }
